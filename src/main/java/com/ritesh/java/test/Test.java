1c12acb849b5f19ad8346dbf1965264d5fb7457a
Ritesh Kumar <ritesh705@gmail.com>
2022-08-29 16:11:22 +0530

    update

--- src/main/java/com/ritesh/java/test/Test.java
+++ src/main/java/com/ritesh/java/test/Test.java
@@ -2,7 +2,8 @@ package com.ritesh.java.test;
 
 public class Test
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("abc".equalsIgnoreCase("abc"));
         System.out.println("abc".equalsIgnoreCase(null));
     }
