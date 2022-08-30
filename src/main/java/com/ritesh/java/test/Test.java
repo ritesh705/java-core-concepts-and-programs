d5905e50fb0f3555972b84a0b4dbcde4b9e2f424
Ritesh Kumar <ritesh705@gmail.com>
2022-08-31 00:24:10 +0530

    update

--- src/main/java/com/ritesh/java/test/Test.java
+++ src/main/java/com/ritesh/java/test/Test.java
@@ -4,7 +4,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("abc".equalsIgnoreCase("abc"));
-        System.out.println("abc".equalsIgnoreCase(null));
+
     }
 }
